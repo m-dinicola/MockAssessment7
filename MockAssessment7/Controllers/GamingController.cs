c9be4456715784620f24c7780c14816005fcbe46
mdinicola <the.m.dinicola@gmail.com>
2021-08-03 11:36:49 -0400

    commit 0

--- MockAssessment7/Controllers/GamingController.cs
+++ MockAssessment7/Controllers/GamingController.cs
@@ -14,6 +14,54 @@ namespace MockAssessment7.Controllers
     {
         GameDB DB = new GameDB();
 
+        [HttpGet("players")]
+        public List<Player> GetPlayers()
+        {
+            return DB.Players.ToList();
+        }
 
+        [HttpGet("classes")]
+        public List<PlayerClass> GetClasses()
+        {
+            return DB.PlayerClasses.ToList();
+        }
+
+        [HttpGet("PlayersMinLevel/{level}")]
+        public List<Player> GetPlayersAboveLevel(int level)
+        {
+            return DB.Players.Where(x => x.Level >= level).ToList();
+        }
+
+        [HttpGet("PlayersSortLevel")]
+        public List<Player> GetSortedPlayers()
+        {
+            return DB.Players.OrderBy(x=>x.Level).Reverse().ToList();
+        }
+
+        [HttpGet("PlayersOfClass/{playerClass}")]
+        public List<Player> GetFromClass(string playerClass)
+        {
+            return DB.Players.Where(x => x.CurrentClass.Name.ToLower() == playerClass.ToLower()).ToList();
+        }
+
+        [HttpGet("PlayersOfType/{playerType}")]
+        public List<Player> GetFromType(string playerType)
+        {
+            return DB.Players.Where(x => x.CurrentClass.Type.ToLower() == playerType.ToLower()).ToList();
+        }
+
+        [HttpGet("AllPlayedClasses")]
+        public List<PlayerClass> GetPlayedClasses()
+        {
+            List<PlayerClass> playedClasses = new List<PlayerClass>();
+            foreach(PlayerClass _class in DB.PlayerClasses)
+            {
+                if (DB.Players.Exists(x => x.CurrentClass == _class))
+                {
+                    playedClasses.Add(_class);
+                }
+            }
+            return playedClasses;
+        }
     }
 }
